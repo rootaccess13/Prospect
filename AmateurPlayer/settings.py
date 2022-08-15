1e9b709c9a0c83c31a0e61c27b8f2ca016f76003
root <mavericktena@gmail.com>
2022-08-15 21:46:31 +0800

    assets

--- AmateurPlayer/settings.py
+++ AmateurPlayer/settings.py
@@ -174,6 +174,5 @@ AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-django_heroku.settings(locals())
 
 CORS_ORIGIN_ALLOW_ALL = True
