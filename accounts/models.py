840caadfc980c174665fa0ac2a7667d4525e736c
root <mavericktena@gmail.com>
2022-08-31 01:08:30 +0800

    tetet

--- accounts/models.py
+++ accounts/models.py
@@ -47,8 +47,6 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(
-        primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(_('email address'), unique=True)
     ign = models.CharField(max_length=50, blank=True, null=True)
     avatar = models.ImageField(
