c01012e7643b9017a0a16b8493c0fc7127c16b45
root <mavericktena@gmail.com>
2022-08-31 00:30:38 +0800

    fuck

--- accounts/models.py
+++ accounts/models.py
@@ -47,7 +47,8 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(_('email address'), unique=True)
     ign = models.CharField(max_length=50, blank=True, null=True)
     avatar = models.ImageField(
