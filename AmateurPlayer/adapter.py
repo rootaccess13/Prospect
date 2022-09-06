e88d7190a7f3585fdd9910a0db9a3a49f2442106
root <mavericktena@gmail.com>
2022-09-07 02:09:34 +0800

    Facebook AuthData

--- AmateurPlayer/adapter.py
+++ AmateurPlayer/adapter.py
@@ -9,6 +9,7 @@ from django.dispatch import receiver
 from django.shortcuts import redirect
 from django.conf import settings
 import json
+from allauth.account.signals import user_signed_up
 
 
 class MyAccountAdapter(DefaultAccountAdapter):
@@ -28,6 +29,15 @@ class MyAccountAdapter(DefaultAccountAdapter):
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
 
+    def pre_social_login(self, request, sociallogin):
+        # check if user is already had an account
+        if request.user.is_authenticated:
+            sociallogin.connect(request, request.user)
+            raise ImmediateHttpResponse(
+                redirect("/info/{pk}/".format(pk=request.user.pk)))
+
+        return super().pre_social_login(request, sociallogin)
+
     def get_connect_redirect_url(self, request, socialaccount):
         path = "/info/{pk}/"
         # check if user is already had an account
@@ -40,89 +50,9 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
         print("Signed In : " + request.user.ign)
         return path
 
-    def pre_social_login(self, request, sociallogin):
-        if sociallogin.account.provider == 'google':
-            # get user model
-            User = get_user_model()
-            # get user email
-            email = sociallogin.account.extra_data['email']
-            # get user by email
-            user = User.objects.filter(email=email).first()
-            # check if user is already had an account
-            if user:
-                # login user
-                perform_login(request, user, email_verification='optional')
-                # redirect to home page
-                raise ImmediateHttpResponse(redirect('/'))
-        if sociallogin.account.provider == 'facebook':
-            # get user model
-            User = get_user_model()
-            # get user email
-            email = sociallogin.account.extra_data['email']
-            # get user by email
-            user = User.objects.filter(email=email).first()
-            # check if user is already had an account
-            if user:
-                # login user
-                perform_login(request, user, email_verification='optional')
-                # redirect to home page
-                raise ImmediateHttpResponse(redirect('/'))
 
-    def populate_user(self, request, sociallogin, data):
-        # get user model
-        User = get_user_model()
-        # get user email
-        email = sociallogin.account.extra_data['email']
-        # get user by email
-        user = User.objects.filter(email=email).first()
-        # check if user is already had an account
-        if user:
-            # set user
-            sociallogin.connect(request, user)
-            # redirect to home page
-            raise ImmediateHttpResponse(redirect('/'))
+@receiver(user_signed_up)
+def user_signed_up_(request, user, **kwargs):
+    print("Signed Up : " + user.ign)
 
-    def save_user(self, request, sociallogin, form=None):
-        # get user model
-        User = get_user_model()
-        # get user email
-        email = sociallogin.account.extra_data['email']
-        # get user by email
-        user = User.objects.filter(email=email).first()
-        # check if user is already had an account
-        if user:
-            # set user
-            sociallogin.connect(request, user)
-            # redirect to home page
-            raise ImmediateHttpResponse(redirect('/'))
-
-
-@receiver(pre_social_login)
-def link_to_local_user(sender, request, sociallogin, **kwargs):
-    ''' Login and redirect
-    This is done in order to tackle the situation where user's email retrieved
-    from one provider is different from already existing email in the database
-    (e.g facebook and google both use same email-id). Specifically, this is done to
-    tackle following issues:
-    * https://github.com/pennersr/django-allauth/issues/215
-
-    '''
-    # get user model
-    user_model = get_user_model()
-    # get email from sociallogin
-    email = sociallogin.account.extra_data.get('email')
-    # get user with this email
-    user = user_model.objects.filter(email=email).first()
-    # if user exists
-    if user:
-        # login user
-        perform_login(request, user, email_verification='none')
-        # redirect to home
-        raise ImmediateHttpResponse(
-            redirect('completeinfo', pk=request.user.pk))
-    # if user does not exist
-    else:
-        # log error message
-
-        # redirect to signup page
-        raise ImmediateHttpResponse(redirect('/'))
+    return redirect("/info/{pk}/".format(pk=user.pk))
